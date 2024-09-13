584a6e9e4f44a6b46f537c4f64280ab11ffae90b
mnha <mnha@nexreal.com>
2024-09-13 16:33:32 +0900

    Dev : 실행 인자 추가
      # 실행 시 인자 추가

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"bigtable/internal/node"
 	"bigtable/internal/rest"
+	"flag"
+	"fmt"
 	"log"
+	"path/filepath"
 )
 
 func main() {
 	
-	kvNode, err := node.NewKVNode("kv_data")
+	dbPath := flag.String("db", "kv_data", "Path to the database directory")
+
+	port := flag.Int("port", 6195, "Port number for the server")
+	flag.Parse()
+
+
+	absDbPath, err := filepath.Abs(*dbPath)
+	if err != nil {
+		log.Fatalf("Failed to get absolute path: %v", err)
+	}
+
+	kvNode, err := node.NewKVNode(absDbPath)
 
 	if err !=nil{
 		log.Fatalf("Failed to create KVnode: %v", err)
@@ -19,8 +33,9 @@ func main() {
 	kvService := rest.NewKVStoreService(kvNode)
 
 	server := rest.NewServer(kvService)
-
-	if err := server.Start(":6195"); err != nil{
+	address := fmt.Sprintf(":%d", *port)
+	log.Printf("Starting server on %s with database at %s", address, absDbPath)
+	if err := server.Start(address); err != nil{
 		log.Fatalf("Server failed to start : %v", err)
 	}
 
